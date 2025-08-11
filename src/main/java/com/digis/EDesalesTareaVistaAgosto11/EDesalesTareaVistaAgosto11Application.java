c858e6efa603a7eca3e01aafecd9af6a7da07bd6
Alien 13 <Alien 13@ALIEN13>
2025-08-11 17:29:50 -0600

    Comienzo Proyecto

---
+++ src/main/java/com/digis/EDesalesTareaVistaAgosto11/EDesalesTareaVistaAgosto11Application.java
@@ -0,0 +1,13 @@
+package com.digis.EDesalesTareaVistaAgosto11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDesalesTareaVistaAgosto11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDesalesTareaVistaAgosto11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/digis/EDesalesTareaVistaAgosto11/EDesalesTareaVistaAgosto11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digis.EDesalesTareaVistaAgosto11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDesalesTareaVistaAgosto11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
