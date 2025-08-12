971de89f955b38ffcbed03ba84f39996872f0658
Alien 13 <Alien 13@ALIEN13>
2025-08-11 19:26:05 -0600

    Proyecto armado en arquitectura

---
+++ src/main/java/com/digis/EDesalesTareaVistaAgosto11/Controller/ControllerPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.digis.EDesalesTareaVistaAgosto11.Controller;
+
+/**
+ *
+ * @author Alien 13
+ */
+public class ControllerPrincipal {
+    
+}
---
+++ src/main/java/com/digis/EDesalesTareaVistaAgosto11/ML/Result.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.digis.EDesalesTareaVistaAgosto11.ML;
+
+import java.util.List;
+
+/**
+ *
+ * @author Alien 13
+ */
+public class Result<T> {
+
+    public boolean correct;
+    public String errorMessage;
+    public Exception ex;
+    public T object;
+    public List<T> objects;
+}
